e77a3ef60481bdcb02ba2875ca6b4201c1f4c82e
quangthangit <thangtq.it@gmail.com>
2025-10-14 12:02:19 +0700

    fix: import card

--- src/pages/matching-game-4x3/GameScreen/GameScreen.jsx
+++ src/pages/matching-game-4x3/GameScreen/GameScreen.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import style from "./style.module.css";
-import Card from "../card/card";
+import Card from "../card/Card";
 
 export default function GameScreen({ data, onBack }) {
   const [opened, setOpened] = useState([]);
